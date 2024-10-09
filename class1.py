e6a50259736401869d7aed5eedb6a8fbd44a27c7
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 <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>
2024-10-09 09:29:54 +0100

    my first python class

---
+++ class1.py
@@ -0,0 +1,68 @@
+from math import *
+print(sqrt(2948.59))
+product = '144hz 30in computer'
+cost = '$400'
+price_in_cents = 1600
+names = "jeremy"
+delivery_country="miami"
+age = "17yrs"
+authurization_number= 5230487011
+identification_licensing_documents= "NIN, drivers liscence"
+postal_code= 102333
+request_country= "nigeria"
+
+num1= 46
+num2=20
+numm3 = 30
+sum = num1 + num2
+print(sum)
+print(cost)
+num3=9
+num4 = 16
+num5 = 5
+product = num4 * num5
+division= num3 / num5
+print(product)
+print(division)
+modulus = num1 % num2
+round = num1// num4
+print(round)
+print(modulus)
+float_num = 30,9 #numbers with decumals
+float(20.9)
+string_value= "my name is falade temiloluwa jeremiah"
+print(string_value)
+s = ' and favour'
+print( string_value + s)
+string_value.upper
+print(string_value.replace('j', 'm')) # replaces the string letter j with m
+print(string_value.upper()) # convert the string to upper case 
+print(string_value.lower()) #converts the string to lower case
+print(string_value.islower()) # checks if the string is in lower case 
+print(string_value.isupper()) #checks if the string is in upper case 
+print(string_value.__len__()) #length of the string
+
+
+# how to work  with methods is { Name of Varable.Methods()}
+# how you collect data information from a user {Variable Name = input('prompt')}
+
+#inputs
+
+num7 = int(input("put a number that would be added"))
+num8 = int(input( "2nd to be added"))
+add = num7 + num8
+sub = num7 - num8
+print (f"the sum of your input is{add}")
+print (f"the subtraction of your input is{sub}") # the funcyion of here is for concantenation of strings and variabbles for output
+print(str(num7))# convert number to string
+print(abs(-78))# output 78 this is the maximum value of the numbers 
+print(max(1,2,3,4,5))#heighest value number
+print(min(1,2,3,4,5))#lowest value numbe
+print(round(3.14))# answer3 rounds the number up 
+print(bin(78))# 01100011 converts number to binary
+print(hex(78))#0x10b converts number  to hexadecimal
+
+
+
+
+
---
+++ class1prc.py
@@ -0,0 +1,6 @@
+from math import *
+_num1 = int(input("what is the first number that u wannt to be added"))
+_num2 = int( input("what is the second number to be added"))
+sum = _num1 + _num2
+print (sum)
+
