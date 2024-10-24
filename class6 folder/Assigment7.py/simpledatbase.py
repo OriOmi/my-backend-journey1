c3b19e3309a2042160519e993b96606df277d9e9
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 <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>
2024-10-24 19:23:27 +0100

    SIMPLE DATABASE WITH DICTIONARIES

--- class6 folder/Assigment7.py/simpledatbase.py
+++ class6 folder/Assigment7.py/simpledatbase.py
@@ -1,16 +1,19 @@
+# This is a simple sign up database
+print('This is a simple sign up database')
 User_Database = {
   'Username' : 'jeremy',
 }
   
 
 def User_Registration () :
-  User_Database['Username']= definition2 = input('Input Username ')
-  while  definition2 == User_Database['Username']:
-    if definition2 == User_Database['Username']:
-      print('Username already exists!')
-      User_Database['Username']=input('Input Another Username ')
-    else:
-      break
+  definition1 = User_Database['Username'] # at this line instace definition1 = jeremy {importat lie of code }
+  User_Database['Username']= definition2 = input('Input Username ') # at this second line instance definition2 = new username
+  while  definition2 == definition1: # if definition2 == definition1 code continues to run until else statement(reak statement)
+      if definition2 == User_Database['Username']:
+        print('Username already exists!') #code prints this if username already exists 
+        User_Database['Username']=input('Input Another Username ')
+      else: #if userame doesnt already exist it automatically exits functiion cause o break statemets
+        break 
     
   User_Database['Password']=input('Input Password')
   print('Congratulations you have been successfully Registered')
